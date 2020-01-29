0fb5a3a10eeef0ad5b0d36a75611a2053bc187fa
Nils Kucza <nkucza@cit-ec.uni-bielefeld.de>
2020-01-29 10:59:53 +0100

    call c executable and listen to stdout

--- node_helper.js
+++ node_helper.js
@@ -1,5 +1,6 @@
 'use strict';
 const NodeHelper = require('node_helper');
+const { spawn } = require('child_process');
 
 const {PythonShell} = require('python-shell');
 var cAppStarted = false
@@ -7,9 +8,28 @@ var cAppStarted = false
 module.exports = NodeHelper.create({
 
 	cApp_start: function () {
-		const self = this;		
+		const self = this;
+		console.log('starting c object detection');
+		console.log('modules/' + this.name + '/object-detection/build/object_detection');
+		self.objectDet = spawn('modules/' + this.name + '/object-detection/build/object_detection',['modules/' + this.name + '/object-detection/build']);
+		self.objectDet.stdout.on('data', (data) => {
+			try{
+				var parsed_message = JSON.parse(`${data}`)
 
-		
+				if (parsed_message.hasOwnProperty('DETECTED_OBJECTS')){
+					//console.log("[" + self.name + "] detected object: " + parsed_message.detected.name + " center in "  + parsed_message.detected.center);
+					self.sendSocketNotification('DETECTED_OBJECTS', parsed_message);
+				}else if (parsed_message.hasOwnProperty('OBJECT_DET_FPS')){
+					//console.log("[" + self.name + "] object detection fps: " + JSON.stringify(parsed_message));
+					self.sendSocketNotification('OBJECT_DET_FPS', parsed_message.OBJECT_DET_FPS);
+				}
+			}
+			catch(err) {
+				
+				//console.log(err)
+			}
+  			//console.log(`stdout: ${data}`);
+		});	
   	},
 
   	// Subclass socketNotificationReceived received.
@@ -31,13 +51,7 @@ module.exports = NodeHelper.create({
   	},
 
 	stop: function() {
-		const self = this;
-		//self.obj_pyshell.childProcess.kill('SIGKILL');
-		//self.obj_pyshell.end(function (err) {
-           	if (err){
-        		//throw err;
-    		};
-    		console.log('finished');
-		});
+		const self = this;	
+		
 	}
 });
