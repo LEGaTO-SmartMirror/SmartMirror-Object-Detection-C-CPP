ba0f94ca757f02290a4ce31be9a6d7b4125d5e69
Nils Kucza <nkucza@cit-ec.uni-bielefeld.de>
2020-08-04 11:43:09 +0200

    split falsly joined stdout messages

--- node_helper.js
+++ node_helper.js
@@ -10,25 +10,34 @@ module.exports = NodeHelper.create({
 		const self = this;
 		self.objectDet = spawn('modules/' + this.name + '/object-detection/build/object_detection',['modules/' + this.name + '/object-detection/build', self.config.image_width, self.config.image_height]);
 		self.objectDet.stdout.on('data', (data) => {
-			try{
-				var parsed_message = JSON.parse(`${data}`)
+			
+
+			var data_chunks = `${data}`.split('\n');
+			data_chunks.forEach( chunk => {
+
+				if (chunk.length > 0) {
+	
+				try{
+					var parsed_message = JSON.parse(chunk)
 
 				if (parsed_message.hasOwnProperty('DETECTED_OBJECTS')){
 					//console.log("[" + self.name + "] detected object: " + parsed_message.detected.name + " center in "  + parsed_message.detected.center);
 					self.sendSocketNotification('DETECTED_OBJECTS', parsed_message);
 				}else if (parsed_message.hasOwnProperty('OBJECT_DET_FPS')){
-					//console.log("[" + self.name + "] object detection fps: " + JSON.stringify(parsed_message));
+					console.log("[" + self.name + "] " + JSON.stringify(parsed_message));
 					self.sendSocketNotification('OBJECT_DET_FPS', parsed_message.OBJECT_DET_FPS);
 				}else if (parsed_message.hasOwnProperty('STATUS')){
 					console.log("[" + self.name + "] status received: " + JSON.stringify(parsed_message));
 				}
 			}
-			catch(err) {
-				
-				//console.log(err)
-			}
-  			//console.log(`stdout: ${data}`);
+				catch(err) {	
+					//console.log(err)
+				}
+  				//console.log(chunk);
+				}
+			});
 		});	
+	
 		exec(`renice -n 20 -p ${self.objectDet.pid}`,(error,stdout,stderr) => {
 				if (error) {
 					console.error(`exec error: ${error}`);
